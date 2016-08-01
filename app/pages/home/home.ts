97b276ad3e2d0a44a3b0ba051989a1c75cd3b374
zwq <zwq>
2016-08-01 15:12:16 +0800

    update for popupover page & data-transfer

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,20 +1,34 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {NewPage} from '../new-page/new-page';
-import { ConferenceData } from '../../providers/conference-data';
+import {ConferenceData } from '../../providers/conference-data';
+import {PopoverPage} from '../result/result';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+  // @ViewChild('popoverContent', {read: ElementRef}) content: ElementRef;
+  // @ViewChild('popoverText', {read: ElementRef}) text: ElementRef;
+
+  testRadioOpen: boolean;
+  testRadioResult;
+
 	nextPageInfo:any;
-	questionNo=0;
+	questionNo = 0;
+
   constructor(private navCtrl: NavController, confData: ConferenceData) {
-  	confData.getContents(this.questionNo).then(confData2=>{
-  		this.nextPageInfo=confData2;
+  	confData.getContents(this.questionNo).then(nextPage=>{
+  		this.nextPageInfo = nextPage;
   	});
   }
   nextPage(){
   	this.navCtrl.push(NewPage, this.nextPageInfo);
-  }
+  } 
+
+  // presentPopover(ev) {
+  //   let popover = Popover.create(PopoverPage, this.nextPageInfo);
+
+  //   this.navCtrl.present(popover,{ev:ev});
+  // }  
 }
