7754527e6971db6041f24ccbdcc2e004406dfa24
zwq <zwq>
2016-08-01 14:55:56 +0800

    update for popupover page & data-transfer

---
+++ app/pages/result/result.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController , NavParams} from 'ionic-angular';
+
+import {ConferenceData } from '../../providers/conference-data';
+/*
+  Generated class for the ResultPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/result/result.html'
+})
+export class PopoverPage {
+	pageInfo=0;
+  pageinfoPop:any;
+  testText;
+
+  constructor(private nav: NavController, confData: ConferenceData,param:NavParams) {
+  	// var params;
+	// params = param.data;
+	this.pageinfoPop = param.data;
+  }
+
+}
