ce32477107cc1fbfe31f2a9e3050e539c74fda86
zwq <zwq>
2016-08-04 09:41:33 +0800

    update for .scss

--- app/pages/new-page/new-page.ts
+++ app/pages/new-page/new-page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
-import { NavController, NavParams,Page } from 'ionic-angular';
+import { Popover,NavController, NavParams,Page } from 'ionic-angular';
 
 import { ConferenceData } from '../../providers/conference-data';
+import {PopoverPage} from '../result/result';
 /*
   Generated class for the NewPagePage page.
 
@@ -15,6 +15,7 @@ import { ConferenceData } from '../../providers/conference-data';
 export class NewPage {
 	pageInfo: any;
 	nextPageInfo:any;
+  nextPageIndex:any;
 	// pageCnt:any;
 	// pageInfos=[];
 
@@ -23,23 +24,70 @@ export class NewPage {
   	// this.pageInfo2 = aa[1];
   	this.pageInfo = navParams.data;
   	var pageCnt = navParams.data.soft;
+    this.nextPageIndex = pageCnt;
 
-	confData.getContents(pageCnt).then(confData1=>{
-  		this.nextPageInfo=confData1;
+	  confData.getContents(pageCnt).then(nextPage=>{
+  		this.nextPageInfo = nextPage;
   	});
   }
 
   nextPage(){
-  	this.nav.push(NewPage, this.nextPageInfo);
+    // this.choice(ev);
+    if (this.nextPageIndex == 5){
+      this.presentPopover(event,this.confData);
+    } else {
+    	this.nav.push(NewPage, this.nextPageInfo);
+    }
   }
 
-  choice(){
+  choice(ev){
+    //
+    ev.target.value;
+    var choiceIndex = 0;
+    this.confData.setCheckAndTrue(this.nextPageIndex - 1,choiceIndex);
+    // confData.setCheckAndTrue(this.pageInfo.soft,1)
     //获得选择的选项并记录到对象的相对数据中
         // cont_json[i-1].checked = $('.z2 .cont .checkbox').index($(this));
         // if(cont_json[i-1].checked==cont_json[i-1].true) cont_json[i-1].istrue=1;
         // $(this).find($('.checked')).show();
         // $(this).siblings().find($('.checked')).hide();
         // $('.z2 button.next_question').css('display','block');
+  }
+
+    presentPopover(ev,confData: ConferenceData) {
+      var resultPage; 
+
+      var allQuestions;
+      var temp=0;
+
+      // allQuestions = confData.getContentsAll();
+      confData.getContentsAll().then(result=>{
+         // this.nextPageInfo=result;
+         allQuestions = result;
+        for(var i = 0;i<5;i++){
+          if(allQuestions[i].istrue==1) 
+            temp++;
+        }
+       var score = temp*20;
+
+
+        if(score<40) {
+          resultPage = {"score":score,"img":'image/answer_result_1.png',"correct":temp};
+        }
+        else if(score>=40&&score<=80){
+          resultPage = {"score":score,"img":'image/answer_result_2.png',"correct":temp};          
+        }
+        else{
+          resultPage = {"score":score,"img":'image/answer_result_3.png',"correct":temp};
+        }
+        // 创建结果页
+        // let popover = Popover.create(PopoverPage, resultPage);
+        // this.nav.present(popover,{ev:ev});
+
+        this.nav.push(PopoverPage, resultPage);
+      });
+      
+
   }
 
 }
--- app/providers/conference-data.ts
+++ app/providers/conference-data.ts
@@ -2,8 +2,6 @@
 
 import { Http } from '@angular/http';
 
-// import { UserData } from './user-data';
-
 
 @Injectable()
 export class ConferenceData {
@@ -34,133 +32,26 @@ export class ConferenceData {
     });
   }
 
-  // processData(data) {
-  //   // just some good 'ol JS fun with objects and arrays
-  //   // build up the data by linking speakers to sessions
-
-  //   data.tracks = [];
-
-  //   // loop through each day in the schedule
-  //   data.schedule.forEach(day => {
-  //     // loop through each timeline group in the day
-  //     day.groups.forEach(group => {
-  //       // loop through each session in the timeline group
-  //       group.sessions.forEach(session => {
-  //         this.processSession(data, session);
-  //       });
-  //     });
-  //   });
-
-  //   return data;
-  // }
-
-  // processSession(data, session) {
-  //   // loop through each speaker and load the speaker data
-  //   // using the speaker name as the key
-  //   session.speakers = [];
-  //   if (session.speakerNames) {
-  //     session.speakerNames.forEach(speakerName => {
-  //       let speaker = data.speakers.find(s => s.name === speakerName);
-  //       if (speaker) {
-  //         session.speakers.push(speaker);
-  //         speaker.sessions = speaker.sessions || [];
-  //         speaker.sessions.push(session);
-  //       }
-  //     });
-  //   }
-
-  //   if (session.tracks) {
-  //     session.tracks.forEach(track => {
-  //       if (data.tracks.indexOf(track) < 0) {
-  //         data.tracks.push(track);
-  //       }
-  //     });
-  //   }
-  // }
-
-  // getTimeline(dayIndex, queryText = '', excludeTracks = [], segment = 'all') {
-  //   return this.load().then(data => {
-  //     let day = data.schedule[dayIndex];
-  //     day.shownSessions = 0;
-
-  //     queryText = queryText.toLowerCase().replace(/,|\.|-/g, ' ');
-  //     let queryWords = queryText.split(' ').filter(w => !!w.trim().length);
-
-  //     day.groups.forEach(group => {
-  //       group.hide = true;
-
-  //       group.sessions.forEach(session => {
-  //         // check if this session should show or not
-  //         this.filterSession(session, queryWords, excludeTracks, segment);
-
-  //         if (!session.hide) {
-  //           // if this session is not hidden then this group should show
-  //           group.hide = false;
-  //           day.shownSessions++;
-  //         }
-  //       });
-
-  //     });
-
-  //     return day;
-  //   });
-  // }
-
-  // filterSession(session, queryWords, excludeTracks, segment) {
-
-  //   let matchesQueryText = false;
-  //   if (queryWords.length) {
-  //     // of any query word is in the session name than it passes the query test
-  //     queryWords.forEach(queryWord => {
-  //       if (session.name.toLowerCase().indexOf(queryWord) > -1) {
-  //         matchesQueryText = true;
-  //       }
-  //     });
-  //   } else {
-  //     // if there are no query words then this session passes the query test
-  //     matchesQueryText = true;
-  //   }
-
-  //   // if any of the sessions tracks are not in the
-  //   // exclude tracks then this session passes the track test
-  //   let matchesTracks = false;
-  //   session.tracks.forEach(trackName => {
-  //     if (excludeTracks.indexOf(trackName) === -1) {
-  //       matchesTracks = true;
-  //     }
-  //   });
-
-  //   // if the segement is 'favorites', but session is not a user favorite
-  //   // then this session does not pass the segment test
-  //   let matchesSegment = false;
-  //   if (segment === 'favorites') {
-  //     if (this.user.hasFavorite(session.name)) {
-  //       matchesSegment = true;
-  //     }
-  //   } else {
-  //     matchesSegment = true;
-  //   }
-
-  //   // all tests must be true if it should not be hidden
-  //   session.hide = !(matchesQueryText && matchesTracks && matchesSegment);
-  // }
-
   getContents(cnt) {
     return this.load().then(data => {
       return data.content[cnt];
     });
   }
 
-  // getTracks() {
-  //   return this.load().then(data => {
-  //     return data.tracks.sort();
-  //   });
-  // }
+  getContentsAll() {
+    return this.load().then(data => {
+      return data.content;
+    });
+  }
 
-  // getMap() {
-  //   return this.load().then(data => {
-  //     return data.map;
-  //   });
-  // }
+  setCheckAndTrue(cnt,checkIndex){
+    return this.load().then(data => {
+      data.content[cnt].checked = checkIndex;
+      if (data.content[cnt].checked == data.content[cnt].true){
+        data.content[cnt].istrue=1;
+      }
+
+    });   
+  }
 
 }
