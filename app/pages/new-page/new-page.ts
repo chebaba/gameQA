49e451f0acd22148b393ed1b1970561b68ec7455
zwq <zwq>
2016-08-10 15:51:00 +0800

    update for css-changed & nextButton

--- app/pages/new-page/new-page.ts
+++ app/pages/new-page/new-page.ts
@@ -21,22 +21,26 @@ export class NewPage {
   constructor(private nav: NavController, private confData: ConferenceData, private navParams:NavParams) {
 
   	this.pageInfo = navParams.data;
+    // 选项情况初期载入
     var checked = navParams.data.checked;
-
-    this.dataObject={"option1":false,"option2":false,"option3":false,"option4":false};
+    this.dataObject={"option1":false,"option2":false,"option3":false,"option4":false,"button":true};
     switch(checked){
-      case"0":this.dataObject.option1=true;
+      case"0":
+      this.dataObject.option1=true;
+      this.dataObject.button=false;
       break;
       case"1":this.dataObject.option2=true;
+      this.dataObject.button=false;
       break;
       case"2":this.dataObject.option3=true;
+      this.dataObject.button=false;
       break;
       case"3":this.dataObject.option4=true;
+      this.dataObject.button=false;
       break;
       default:
     }
-
-
+    // 下一题信息载入
     var nextPageCnt = navParams.data.soft;
     this.nextPageIndex = nextPageCnt;
 
@@ -47,7 +51,6 @@ export class NewPage {
   }
 
   nextPage(){
-    // this.choice(ev);
     if (this.nextPageIndex == 5){
       this.presentPopover(event,this.confData);
     } else {
@@ -58,38 +61,37 @@ export class NewPage {
   choice(ev){
     var choiceIndex = ev.currentTarget.attributes[0].value;
     this.confData.setCheckAndTrue(this.nextPageIndex - 1,choiceIndex);
+    this.dataObject.button=false;
   }
 
-    presentPopover(ev,confData: ConferenceData) {
-      var resultPage; 
+  presentPopover(ev,confData: ConferenceData) {
+    var resultPage; 
 
-      var allQuestions;
-      var temp=0;
+    var allQuestions;
+    var temp=0;
 
-      confData.getContentsAll().then(result=>{
+    confData.getContentsAll().then(result=>{
 
-        allQuestions = result;
-        for(var i = 0;i<5;i++){
-          if(allQuestions[i].istrue==1) 
-            temp++;
-        }
-       var score = temp*20;
+      allQuestions = result;
+      for(var i = 0;i<5;i++){
+        if(allQuestions[i].istrue==1) 
+          temp++;
+      }
+     var score = temp*20;
 
 
-        if(score<40) {
-          resultPage = {"score":score,"img":'image/answer_result_1.png',"correct":temp};
-        }
-        else if(score>=40&&score<=80){
-          resultPage = {"score":score,"img":'image/answer_result_2.png',"correct":temp};          
-        }
-        else{
-          resultPage = {"score":score,"img":'image/answer_result_3.png',"correct":temp};
-        }
+      if(score<40) {
+        resultPage = {"score":score,"img":'image/answer_result_1.png',"correct":temp};
+      }
+      else if(score>=40&&score<=80){
+        resultPage = {"score":score,"img":'image/answer_result_2.png',"correct":temp};          
+      }
+      else{
+        resultPage = {"score":score,"img":'image/answer_result_3.png',"correct":temp};
+      }
 
-        this.nav.push(PopoverPage, resultPage);
-      });
-      
-
+      this.nav.push(PopoverPage, resultPage);
+    });
   }
 
 }
